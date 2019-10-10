3f067f5b48cfa5a064e668a0e7cb773e2d4de40e
Huda Yousif <hjdoski@gmail.com>
2019-10-10 22:47:51 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,3 +1,9 @@
-def prime? n
-  (2..Math.sqrt(n)).none? {|f| n % f == 0}
+def prime? (n)
+    if n <= 1
+        false
+    elsif n == 2
+        true
+    else 
+        (2..n/2).none? { |i| n % i == 0}
+    end
 end
